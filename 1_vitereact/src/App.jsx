836c5beebaf0159236b94c524324fca54ec151d6
Kiran-F <fazaian2004@gmail.com>
2025-09-26 11:16:23 +0500

    learning react day 2

--- 1_vitereact/src/App.jsx
+++ 1_vitereact/src/App.jsx
@@ -1,10 +1,11 @@
 import Addition from './Addition'
 function App() {
+  const username = "Kiran Fatima";
   return (
     //  this is called returning a fragment otherwise it causes error if multiple elements are returned.
     <>
       <Addition />
-      <h2>This text is basically from the app.jsx file...</h2>
+      <h2>This text is basically from the app.jsx file... {username}</h2>
     </>
   )
 }
--- 1_vitereact/src/main.jsx
+++ 1_vitereact/src/main.jsx
@@ -1,9 +1,30 @@
-import { StrictMode } from 'react'
+import { createElement, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+  return (
+      <div>
+        <p>This is the text in the funtion which is in the main.jsx.</p>
+      </div>
+  )
+}
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit google via anotherElement.</a>
+)
+
+//how objects are created from elements in react
+const myName = "Kiran hoon mein";
+const reactElement = createElement(
+  'a', //tagName
+  {href: 'https://google.com', target: '_blank'}, //properties
+  'Click me to visit google.com via reactElement (rendering a react object) ', //the inner text to be displayed
+  myName //if we want to add any variable and it's value.
+)
+
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+    // reactElement
     <App />
-  </StrictMode>,
 )
