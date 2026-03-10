b03bb681fd5a5efe2face6edb821fec19909245d
Kiran-F <fazaian2004@gmail.com>
2026-03-11 00:19:46 +0500

    context api

---
+++ 8_minicontext/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2>Here's context API.</h2>
+    </>
+  )
+}
+
+export default App
---
+++ 8_minicontext/src/context/UserContext.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const UserContext = React.createContext();
+
+export default UserContext;
+
+// first step is to create a context and export it. UserContext is a provider. 
+// Wrap the content with UserContext.It will then act like a provider. 
+// e.g: 
+// <UserContext>
+//     <Data />
+//     <Login />
+//     <Card />
+// </UserContext>
