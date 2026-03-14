8ccf32c48330b4c2f9bf6beeef6d10c0aa73ce57
Kiran-F <fazaian2004@gmail.com>
2026-03-15 01:59:33 +0500

    figured out to install tailwind with vite 8.0.0

---
+++ 10_todoContextLocal/src/App.jsx
@@ -0,0 +1,14 @@
+// import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-orange-500 text-4xl font-bold">
+        Hi there i made it and tailwind is right there BHAI SAAB!
+      </h1>
+    </>
+  )
+}
+
+export default App
--- 9_themeswitcher/src/App.jsx
+++ 9_themeswitcher/src/App.jsx
@@ -1,88 +1,90 @@
-// import { useState } from 'react'
-// import './App.css'
-// import { ThemeProvider } from './context/theme'
-// import ThemeBtn from './components/Themebtn';
-// import Card from './components/Card';
-
-// function App() {
-//   const [themeMode, setThemeMode] = useState("light");
-
-//   const lightTheme =() => {
-//     setThemeMode("light");
-//   }
-
-//   const darkTheme = () => {
-//     setThemeMode("dark");
-//   }
-  
-//     useEffect(() => {
-//     document.querySelector('html').classList.remove("light", "dark")
-//     document.querySelector('html').classList.add(themeMode)
-//   }, [themeMode])
-
-//   return (
-//     <>
-//       <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
-//         <div className="flex flex-wrap min-h-screen items-center">
-//             <div className="w-full">
-//                   <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-//                       <ThemeBtn />
-//                   </div>
-
-//                   <div className="w-full max-w-sm mx-auto">
-//                       <Card />
-//                   </div>
-//             </div>
-//           </div>
-//       </ThemeProvider>
-//     </>
-//   )
-// }
-
-// export default App
-
-
-
-import { useEffect, useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import { ThemeProvider } from './context/theme'
-import ThemeBtn from './components/Themebtn'
-import Card from './components/Card'
+import ThemeBtn from './components/Themebtn';
+import Card from './components/Card';
 
 function App() {
-  const [themeMode, setThemeMode] = useState("light")
+  const [themeMode, setThemeMode] = useState("light");
 
-  const lightTheme = () => {
-    setThemeMode("light")
+  const lightTheme =() => {
+    setThemeMode("light");
   }
 
   const darkTheme = () => {
-    setThemeMode("dark")
+    setThemeMode("dark");
   }
-
-  // actual change in theme
-
-  useEffect(() => {
+  
+    useEffect(() => {
     document.querySelector('html').classList.remove("light", "dark")
     document.querySelector('html').classList.add(themeMode)
-  }, [themeMode])
-  
+    }, [themeMode])
 
   return (
-    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
-      <div className="flex flex-wrap min-h-screen items-center">
-          <div className="w-full">
-              <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                  <ThemeBtn />
-              </div>
+    <>
+      <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+        <div className="flex flex-wrap min-h-screen items-center">
+            <div className="w-full">
+                  <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                      <ThemeBtn />
+                  </div>
 
-              <div className="w-full max-w-sm mx-auto">
-                  <Card />
-              </div>
+                  <div className="w-full max-w-sm mx-auto">
+                      <Card />
+                  </div>
+            </div>
           </div>
-      </div>
-    </ThemeProvider>
+      </ThemeProvider>
+    </>
   )
 }
 
 export default App
+
+//Also will be required to add @custom-variant dark (&:where(.dark, .dark *)); in index.css to enable toggling of themes
+
+
+
+// import { useEffect, useState } from 'react'
+// import './App.css'
+// import { ThemeProvider } from './context/theme'
+// import ThemeBtn from './components/Themebtn'
+// import Card from './components/Card'
+
+// function App() {
+//   const [themeMode, setThemeMode] = useState("light")
+
+//   const lightTheme = () => {
+//     setThemeMode("light")
+//   }
+
+//   const darkTheme = () => {
+//     setThemeMode("dark")
+//   }
+
+//   // actual change in theme
+
+//   useEffect(() => {
+//     document.querySelector('html').classList.remove("light", "dark")
+//     document.querySelector('html').classList.add(themeMode)
+//   }, [themeMode])
+  
+
+//   return (
+//     <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+//       <div className="flex flex-wrap min-h-screen items-center">
+//           <div className="w-full">
+//               <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+//                   <ThemeBtn />
+//               </div>
+
+//               <div className="w-full max-w-sm mx-auto">
+//                   <Card />
+//               </div>
+//           </div>
+//       </div>
+//     </ThemeProvider>
+//   )
+// }
+
+// export default App
