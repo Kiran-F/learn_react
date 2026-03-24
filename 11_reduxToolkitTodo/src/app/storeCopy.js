3b788ad29941a678602e02e721d036a15ee6e97e
Kiran-F <fazaian2004@gmail.com>
2026-03-24 20:01:17 +0500

    created the react project blogApp

--- 11_reduxToolkitTodo/src/app/storeCopy.js
+++ 11_reduxToolkitTodo/src/app/storeCopy.js
@@ -4,3 +4,9 @@ import todoReducer from '../features/todo/todoSliceCopy'
 export const storeCopy = configureStore({
     reducer: todoReducer
 })
+
+// export const storeCopy = configureStore({
+//     reducer:{
+//         todo: todoReducer
+//     } 
+// })
